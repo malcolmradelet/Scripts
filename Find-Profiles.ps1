fb93b6962ea568f0a6ea8a29a508807acf4c320d
malcolmradelet <29856299+malcolmradelet@users.noreply.github.com>
2018-07-12 00:17:43 -0700

    Add variable for profile path

--- Find-Profiles.ps1
+++ Find-Profiles.ps1
@@ -1,10 +1,11 @@
-$UserName = "name2"
+$UserName = "name"
 $DiscoveredProfiles = @()
 $aProfileTypes = @("$UserName";"$UserName"+".v2";"$UserName"+".v6")
+$Path = "C:\Temp\Profiles\"
 
 Foreach ($P in $aProfileTypes) {
-    if(Test-Path -Path ("C:\Temp\Profiles\" + "$P") -PathType Container) {
-        Write-Verbose "$P Folder Found"
+    if(Test-Path -Path (Join-Path -Path $Path -ChildPath $P) -PathType Container) {
+        Write-Verbose "$P Folder Found" -Verbose
         $DiscoveredProfiles += "C:\Temp\Profiles\" + "$P"}
     }
 
